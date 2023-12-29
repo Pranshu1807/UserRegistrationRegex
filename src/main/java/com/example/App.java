ce85b56df264acbbe966f92555907d2fcb1f33b0
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-29 07:12:35 +0530

    UC2 implemented

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,15 +6,19 @@ import java.util.regex.Pattern;
 
 public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         String regexPattern = "^[A-Z][a-zA-z]{2,}$";
         Pattern pattern = Pattern.compile(regexPattern);
 
-        System.out.println("Enter a valid name");
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
+        System.out.println("Enter a valid first name");
+        String firstname = sc.nextLine();
+        Matcher matcher = pattern.matcher(firstname);
+        System.out.println(matcher.matches());
 
-        Matcher matcher = pattern.matcher(name);
+        System.out.println("Enter a valid Last name");
+        String lastName = sc.nextLine();
+        matcher = pattern.matcher(lastName);
         System.out.println(matcher.matches());
 
     }
