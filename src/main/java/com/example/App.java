9a9ec1b01f69bca6f284707becea479524686cf8
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-29 19:51:47 +0530

    UC10 implemented

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,21 +6,21 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // System.out.println("Enter a valid first name");
-        // String firstname = sc.nextLine();
-        // System.out.println(NameValidator.ValidateName(firstname));
+        System.out.println("Enter a valid first name");
+        String firstname = sc.nextLine();
+        System.out.println(NameValidator.ValidateName(firstname));
 
-        // System.out.println("Enter a valid Last name");
-        // String lastName = sc.nextLine();
-        // System.out.println(NameValidator.ValidateName(lastName));
+        System.out.println("Enter a valid Last name");
+        String lastName = sc.nextLine();
+        System.out.println(NameValidator.ValidateName(lastName));
 
-        // System.out.println("Enter the email");
-        // String email = sc.nextLine();
-        // System.out.println(EmailValidator.ValidateEmail(email));
+        System.out.println("Enter the email");
+        String email = sc.nextLine();
+        System.out.println(EmailValidator.ValidateEmail(email));
 
-        // System.out.println("Enter the phone number");
-        // String phone = sc.nextLine();
-        // System.out.println(PhoneValidator.ValidatePhone(phone));
+        System.out.println("Enter the phone number");
+        String phone = sc.nextLine();
+        System.out.println(PhoneValidator.ValidatePhone(phone));
 
         System.out.println("Enter the Password");
         String password = sc.nextLine();
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,20 +1,60 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import org.junit.Test;
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    @ParameterizedTest
+    @ValueSource(strings = { "Pranshu", "PraNshu", "PraPRsad", "Prr" })
+    public void happyTestName(String firstName) {
+        assertEquals(true, NameValidator.ValidateName(firstName));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "pranshu", "9pranshu", "P", "1231-p" })
+    public void sadTestName(String lastName) {
+        assertEquals(false, NameValidator.ValidateName(lastName));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "abc@yahoo.com", "abc-100@yahoo.com", "abc111@abc.com", "abc.100@yahoo.com",
+            "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" })
+    public void happyEmails(String email) {
+        assertEquals(true, EmailValidator.ValidateEmail(email));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "abc",
+            "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com",
+            "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com",
+            "abc@gmail.com.1a", "abc@gmail.com.aa.au" })
+    public void sadEmails(String email) {
+        assertEquals(false, EmailValidator.ValidateEmail(email));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "91 9580281173", "90 1234567890", "90 1111111111" })
+    public void happyPhone(String phone) {
+        assertEquals(true, PhoneValidator.ValidatePhone(phone));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "91 580281173", " 901234567890", "90 11111111112", " 9 1234567890" })
+    public void sadPhone(String phone) {
+        assertEquals(false, PhoneValidator.ValidatePhone(phone));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "Pranshu@1807", "pra@nshuR1", "PRANS1234#", "pranshu12870123#1P" })
+    public void happyPassword(String password) {
+        assertEquals(true, PasswordValidator.ValidatePassword(password));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "Pranshu1807", "pra@nshu1", "PRANSHUdada#", "pranshu12870123#1P@","P1@fifi" })
+    public void sadPassword(String password) {
+        assertEquals(false, PasswordValidator.ValidatePassword(password));
     }
 }
