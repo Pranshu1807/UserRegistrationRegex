cdd6443c9e934607468fab4e86d13e9e0a6407a0
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-29 07:06:55 +0530

    UC1 implemented

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,21 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class App {
+    public static void main(String[] args) {
+
+        String regexPattern = "^[A-Z][a-zA-z]{2,}$";
+        Pattern pattern = Pattern.compile(regexPattern);
+
+        System.out.println("Enter a valid name");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        Matcher matcher = pattern.matcher(name);
+        System.out.println(matcher.matches());
+
     }
 }
