d24220239b4f8cf446ff97bebc4f5fbc69028ae6
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-29 09:23:24 +0530

    UC8 implemented

--- src/main/java/com/example/PasswordValidator.java
+++ src/main/java/com/example/PasswordValidator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class PasswordValidator {
     public static boolean ValidatePassword(String password) {
-        String passwordPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[.,:;@#%$^&{}!()<>/|]).{8,}$";
+        String passwordPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=[a-zA-Z0-9]*[@#$%^&+=!])(?!.*[@#$%^&+=!].*[@#$%^&+=!]).{8,}$";
         Pattern pattern = Pattern.compile(passwordPattern);
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
